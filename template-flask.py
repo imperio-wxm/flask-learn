0e3b625cd1c7162c69aebb6fc21bbbd5abb768e9
wxmimperio <wxmimperio@outlook.com>
2016-02-02 17:14:00 +0800

    "change templates"

---
+++ template-flask.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', title='Hello')
+
+@app.route('/about')
+def about():
+    return 'about'
+
+if __name__ == '__main__':
+    app.run(debug=True)

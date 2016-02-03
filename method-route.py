a929ff72abec7c14989e1c117c1fe52e7442b196
wxmimperio <wxmimperio@outlook.com>
2016-02-03 11:06:20 +0800

    "get form info"

--- method-route.py
+++ method-route.py
@@ -26,7 +26,10 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        print username,password
+    else:
+        username = request.args['username']
+        password = request.args['password']
+    print username,password
     return render_template('login.html',method=request.method)
 if __name__ == '__main__':
     app.run(debug=True)

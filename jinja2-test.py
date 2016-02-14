a54f9f095a13d6b89a68af97864c2fcec5da1907
wxmimperio <wxmimperio@outlook.com>
2016-02-14 17:47:43 +0800

    "change jinjia2-test"

--- jinja2-test.py
+++ jinja2-test.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 __author__ = "wxmimperio"
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from livereload import Server
 
 app = Flask(__name__)
@@ -15,7 +15,12 @@ def jinja_test():
 def about():
     return 'about'
 
+# 创建jinja2测试函数
+@app.template_test('current_link')
+def is_current_link(link):
+    return link['href'] == request.path
+
 if __name__ == '__main__':
     live_server = Server(app.wsgi_app)
     live_server.watch('**/*.*')
-    live_server.serve(open_url=True)
+    live_server.serve(open_url=False)

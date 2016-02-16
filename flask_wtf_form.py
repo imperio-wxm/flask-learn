87abd9213b2cbd0da2dfeb5f0ab38e3b4bc2d8b3
wxmimperio <wxmimperio@outlook.com>
2016-02-16 22:47:58 +0800

    "change submit"

--- flask_wtf_form.py
+++ flask_wtf_form.py
@@ -3,10 +3,11 @@
 __author__ = "wxmimperio"
 
 from flask_wtf import Form
-from wtforms import PasswordField,StringField
+from wtforms import PasswordField,StringField,SubmitField
 from wtforms.validators import DataRequired
 
 class loginForm(Form):
     # 设置字段不为空
-    username = StringField(validators=[DataRequired()])
-    password = PasswordField(validators=[DataRequired()])
+    username = StringField(label=u'用户名：',validators=[DataRequired()])
+    password = PasswordField(label=u'密码：',validators=[DataRequired()])
+    submit = SubmitField(label=u'提交')

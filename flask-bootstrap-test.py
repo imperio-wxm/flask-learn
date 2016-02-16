ec1beae4c0aeb3324237df6bc446ee86f47f8e36
wxmimperio <wxmimperio@outlook.com>
2016-02-16 21:38:28 +0800

    "change navbar"

--- flask-bootstrap-test.py
+++ flask-bootstrap-test.py
@@ -5,10 +5,24 @@ __author__ = "wxmimperio"
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template,request
 from livereload import Server
+from flask_nav import Nav
+from flask_nav.elements import *
 
 app = Flask(__name__)
 # 注册bootstrap
 bootstrap = Bootstrap(app)
+
+nav = Nav()
+
+@nav.navigation()
+def top():
+    return Navbar(
+        u'Imperio',
+         View(u'首页','about'),View(u'关于','about'),
+        View(u'服务','about'),View(u'标签','about'),
+    )
+
+nav.init_app(app)
 
 @app.route('/')
 def flask_bootstrap_test():

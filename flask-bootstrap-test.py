ab7322fd5942dd2ccb428f44e499ab5905ec62c6
wxmimperio <wxmimperio@outlook.com>
2016-02-16 00:25:02 -0800

    "change bootstrap_test"

--- flask-bootstrap-test.py
+++ flask-bootstrap-test.py
@@ -2,11 +2,13 @@
 #-*- coding: utf-8 -*-
 __author__ = "wxmimperio"
 
-from flask_bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap
 from flask import Flask, render_template,request
 from livereload import Server
 
 app = Flask(__name__)
+# 注册bootstrap
+bootstrap = Bootstrap(app)
 
 @app.route('/',methods=['POST','GET'])
 def flask_bootstrap_test():
@@ -15,6 +17,10 @@ def flask_bootstrap_test():
         password = request.form['password']
         return username,password
     return render_template('bootstrap_test.html',title='bootstrap_test',methods=request.method)
+
+@app.route('/about')
+def about():
+    return 'about'
 
 if __name__ == '__main__':
     live_server = Server(app.wsgi_app)

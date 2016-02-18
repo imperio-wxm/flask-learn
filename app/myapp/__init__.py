806b6c2bb61bfb6c5c47162b559be73e2f84555f
wxmimperio <wxmimperio@outlook.com>
2016-02-18 21:53:30 +0800

    "__init__ 注册blueprint"

--- app/myapp/__init__.py
+++ app/myapp/__init__.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 __author__ = "wxmimperio"
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+from auth import auth as auth_blueprint
+from main import main as main_blueprint
+
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config')
+    # 数据库连接
+    app.config.from_pyfile('config')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/flask_db'
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    db.init_app(app)
+    bootstrap.init_app(app)
+
+    # 注册blueprint
+    app.register_blueprint(auth_blueprint,'/auth')
+    app.register_blueprint(main_blueprint)
+
+    return app

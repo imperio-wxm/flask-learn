d0bd47deee1b7ef99372d58c246da6bfad2332de
wxmimperio <wxmimperio@outlook.com>
2016-02-18 20:45:15 +0800

    "添加views"

--- app/myapp/auth/views.py
+++ app/myapp/auth/views.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 __author__ = "wxmimperio"
+
+from flask import render_template
+
+@app.route('/login'.methods=['GET','POST'])
+def login():
+    return render_template('login.html',title=u'登录',form=form)
+
+@app.route('/register'.methods=['GET','POST'])
+def register():
+    return render_template('register.html',title=u'注册')

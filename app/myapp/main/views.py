250790e8a2f7c5c4da1a1c77c41aafaa91d70e39
wxmimperio <wxmimperio@outlook.com>
2016-02-18 21:22:50 +0800

    "修改 main/views"

--- app/myapp/main/views.py
+++ app/myapp/main/views.py
@@ -17,3 +17,11 @@ def about():
 @login_required
 def admin():
     return 'Admin'
+
+@main.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'),404
+
+@main.app_template_test('current_link')
+def is_current_link(link):
+    return link == request.path

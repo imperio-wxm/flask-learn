3648a43c0bc8d3b1991081f68e53f3dcd0435e66
wxmimperio <wxmimperio@outlook.com>
2016-02-23 22:01:24 +0800

    "add main/forms"

--- app/myapp/main/forms.py
+++ app/myapp/main/forms.py
@@ -2,3 +2,7 @@
 #-*- coding: utf-8 -*-
 __author__ = "wxmimperio"
 
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask.ext.pagedown.fields import PageDownField

1dd919ad7e389a9c7de639512170311258eff419
wxmimperio <wxmimperio@outlook.com>
2016-02-27 21:07:47 +0800

    "change __init__.py"

--- app/myapp/__init__.py
+++ app/myapp/__init__.py
@@ -1,36 +1,61 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-__author__ = "wxmimperio"
+# coding: utf-8
 
-from flask import Flask
+from flask import Flask, request
+from os import path
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user
+from flask_pagedown import PageDown
+from flask_gravatar import Gravatar
+from flask_babel import Babel, gettext as _
+from config import config
 
-from auth import auth as auth_blueprint
-from main import main as main_blueprint
+basedir = path.abspath(path.dirname(__file__))
 
 db = SQLAlchemy()
+babel = Babel()
 bootstrap = Bootstrap()
+pagedown = PageDown()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-def create_app():
+
+def create_app(config_name='default'):
     app = Flask(__name__)
-    app.config.from_pyfile('config')
-    # 数据库连接
-    app.config.from_pyfile('config')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@127.0.0.1/flask_db'
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config.from_object(config[config_name])
+
 
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
-    # 注册blueprint
-    app.register_blueprint(auth_blueprint,url_prefix='/auth')
-    app.register_blueprint(main_blueprint)
+    babel.init_app(app)
+    Gravatar(app, size=64)
+
+    from auth import auth as auth_blueprint
+    from main import main as main_blueprint
+
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    app.register_blueprint(main_blueprint, static_folder='static')
+
+    @app.template_test('current_link')
+    def is_current_link(link):
+        return link == request.path
+
+    @babel.localeselector
+    def get_locale():
+        return current_user.locale
 
     return app
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         f = request.files['file']
+#         basepath = path.abspath(path.dirname(__file__))
+#         upload_path = path.join(basepath, 'static/uploads')
+#         f.save(upload_path + '/' + secure_filename(f.filename))
+#         return redirect(url_for('upload'))
+#     return render_template('upload.html')
